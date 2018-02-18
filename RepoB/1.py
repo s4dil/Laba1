f71045b909aeaffef59d75fd6de9f04135389162
s4 <koly-18@mail.ru>
2018-02-18 17:42:31 +0300

    Commit№2

--- RepoB/1.py
+++ RepoB/1.py
@@ -1,2 +1,17 @@
-s = "BINGO. Это тест"
-print(" ".join(s))
+s=[1,2,2,2,2,3,5,5,6,7,7,7,8,3]
+print("Начальный массив: ",s)
+dat4ik=False
+for i in range(1,len(s)):
+    try:
+        if s[i]==s[i-1]:
+            dat4ik=True
+            continue
+        else:
+            if dat4ik==True:
+                s.pop(i-1)
+                dat4ik=False
+            else:
+                continue
+    except:
+        pass
+print("Преобразованный массив: ",s)
